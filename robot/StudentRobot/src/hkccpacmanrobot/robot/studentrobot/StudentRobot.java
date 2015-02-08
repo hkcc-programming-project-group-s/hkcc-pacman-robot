d16107556484a0074ab9b9adaa7e140030a07ff4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-02-09 00:34:52 +0800

    fixed intellij package setting (.iml config file location)

---
+++ robot/StudentRobot/src/hkccpacmanrobot/robot/studentrobot/StudentRobot.java
@@ -0,0 +1,27 @@
+import hkccpacmanrobot.robot.core.Robot;
+
+public class StudentRobot extends Robot {
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void listenToRemoteController() {
+
+	}
+	
+}
