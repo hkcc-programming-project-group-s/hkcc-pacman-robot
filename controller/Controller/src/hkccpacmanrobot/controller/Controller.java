fea4f7fae22c68f8e6c54ea146c1c7ae2035a955
Nicola Ng <ngnicola513@gmail.com>
2015-03-21 21:19:01 +0800

    building controller prototype

---
+++ controller/Controller/src/hkccpacmanrobot/controller/Controller.java
@@ -0,0 +1,8 @@
+package hkccpacmanrobot.controller;
+
+import hkccpacmanrobot.utils.Messenger;
+import hkccpacmanrobot.utils.
+
+public class Controller{
+    public Messenger[MovementCommand] messenger=
+}
