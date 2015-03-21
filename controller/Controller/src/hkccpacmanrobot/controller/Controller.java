d9ae3f69cfb4673369ce4b3eaad7f5b8f3422c07
Nicola Ng <ngnicola513@gmail.com>
2015-03-21 21:29:18 +0800

    built controller prototype (for android app and java application)

--- controller/Controller/src/hkccpacmanrobot/controller/Controller.java
+++ controller/Controller/src/hkccpacmanrobot/controller/Controller.java
@@ -1,8 +1,28 @@
 package hkccpacmanrobot.controller;
 
-import hkccpacmanrobot.utils.Messenger;
-import hkccpacmanrobot.utils.
+import hkccpacmanrobot.utils.message.Messenger;
+import hkccpacmanrobot.utils.message.MovementCommand;
 
-public class Controller{
-    public Messenger[MovementCommand] messenger=
+import java.io.IOException;
+import java.net.Socket;
+
+public class Controller {
+    public final Messenger<MovementCommand> messenger;
+
+    public Controller(Socket socket) {
+        messenger = new Messenger<MovementCommand>(socket);
+    }
+
+    public Controller(String hostname, int port) {
+        try {
+            Socket socket = new Socket(hostname, port);
+            messenger = new Messenger<MovementCommand>(socket);
+        } catch (IOException e) {
+            throw new RuntimeException(e.toString());
+        }
+    }
+
+    public void sendCommand(MovementCommand command) {
+        messenger.sendMessage(command);
+    }
 }
