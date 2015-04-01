41c3c4a6cf40359eb6be9f3c1eddd5bc69d19b75
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-04-02 00:48:20 +0800

    tmp save

--- src/main/scala/edu/hkcc/pacmanrobot/robot/utils/PathFinder.scala
+++ src/main/scala/edu/hkcc/pacmanrobot/robot/utils/PathFinder.scala
@@ -1,21 +1,18 @@
 package edu.hkcc.pacmanrobot.robot.utils
 
+import edu.hkcc.pacmanrobot.robot.utils.PathFinder.Direction.DirectionType
 import edu.hkcc.pacmanrobot.utils.Utils.getTabularSize
 import edu.hkcc.pacmanrobot.utils.map.ObstacleMap
 import edu.hkcc.pacmanrobot.utils.{Point2D, Utils}
 import neuroevolution.geneticalgorithm.GA
 
+import scala.collection.parallel.mutable.ParArray
+
 /**
  * Created by beenotung on 4/1/15.
  */
 object PathFinder {
-  def create(map: ObstacleMap): PathFinder = {
-    //TODO
-
-    null
-  }
-
-  object Direction extends Enumeration {
+    object Direction extends Enumeration {
     type DirectionType = super.Value
     val UP = Value(0)
     val DOWN = Value(1)
@@ -25,23 +22,20 @@ object PathFinder {
     val UP_RIGHT = Value(5)
     val DOWN_LEFT = Value(6)
     val DOWN_RIGHT = Value(7)
-    val bitSize = 8
+    val bitSize = 3
   }
-
 }
 
-class PathFinder(private var map: Array[Array[Boolean]], var LOOP_INTERVAL: Int = 100) extends Thread {
+class PathFinder(private var obstacleMap: Array[Array[Boolean]], var LOOP_INTERVAL: Int = 100) extends Thread {
   var ai: GA = _
   var source = new Point2D[Int](0, 0)
   var destination = new Point2D[Int](0, 0)
 
-  def _map
-
-  def setMap(newMap: Array[Array[Boolean]]) = {
-    //TODO
-    val newSize = getTabularSize(newMap)
-    if (getTabularSize(map) != newSize)
+  def setMap(newObstacleMap: Array[Array[Boolean]]) = {
+    val newSize = getTabularSize(newObstacleMap)
+    if (getTabularSize(obstacleMap) != newSize)
       ai.resize(newSize)
+    obstacleMap=newObstacleMap
   }
 
   def setLocation(source: Point2D[Int], destination: Point2D[Int]) = {
@@ -60,10 +54,22 @@ class PathFinder(private var map: Array[Array[Boolean]], var LOOP_INTERVAL: Int 
   def loop = {
     //TODO
   }
+  def decode(rawCode:Array[Boolean]):Array[Array[DirectionType]]={
+    ParArray.tabulate[DirectionType](obstacleMap.length,obstacleMap(0).length)((x,y)=>{
+
+    })
+    obstacleMap.indices.par.foreach(x=>{
+      obstacleMap(x).indices.par.foreach(y=>{
+        obstacleMap(x)(y)=
+      })
+    })
+  }
+  def eval(rawCode:Array[Boolean]):Double={
+
+  }
 
   def setup = {
-    ai = new GA(POP_SIZE = 32, BIT_SIZE = getTabularSize(map) * PathFinder.Direction.bitSize,
+    ai = new GA(POP_SIZE = 32, BIT_SIZE = getTabularSize(obstacleMap) * PathFinder.Direction.bitSize,
       P_SELECTION=0.25,P_MUTATION = 0.1,A_MUTATION = 0.03,EVAL_FITNESS_FUNCTION = eval,PROBLEM_TYPE = GA.ProblemType.Minimize,LOOP_INTERVAL=100)
   }
-  def eval(rawCode:Array[Double])
 }
