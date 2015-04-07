4401b5a202ac5707eb195827721fbffcab681389
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-04-07 20:10:47 +0800

    finished opengl init (load library)

--- src/test/java/MiniMapTest.java
+++ src/test/java/MiniMapTest.java
@@ -4,8 +4,8 @@ import edu.hkcc.pacmanrobot.controller.gamecontroller.MiniMapWindow;
  * Created by beenotung on 4/7/15.
  */
 public class MiniMapTest {
-    public static void main (String [] args){
-        MiniMapWindow window=new MiniMapWindow();
-
+    public static void main(String[] args) {
+        MiniMapWindow window = new MiniMapWindow();
+        window.run();
     }
 }
